9363f5a0ffaad394a00d5c234efea4a5e9433dbb
majdeddine <majda-jambouri@hotmail.fr>
2017-10-01 14:29:43 +0100

    Full name greeting

---
+++ chap05/ex1.rb
@@ -0,0 +1,8 @@
+#Full name greeting
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your middle name ?"
+middle_name = gets.chomp
+puts "what's your last name ?"
+last_name = gets.chomp
+puts "Hello #{first_name} #{middle_name} #{last_name} !"
