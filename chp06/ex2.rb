d205f7af9edd16fb2e98491ab4945e429f8f220a
majdeddine <majda-jambouri@hotmail.fr>
2017-10-01 16:22:02 +0100

    Table of contents

---
+++ chp06/ex2.rb
@@ -0,0 +1,8 @@
+#Table of contents
+line_width = 60
+left = 30
+right = 20
+puts("Table of Contents".center(line_width))
+puts("Chapter 1:  Getting Started".ljust(left) + "page  1".rjust(right) )
+puts("Chapter 2:  Numbers".ljust(left) + "page  1".rjust(right) )
+puts("Chapter 3:  Letters".ljust(left) + "page  1".rjust(right) )
