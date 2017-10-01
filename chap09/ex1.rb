aac0bae128684461f6996cb177057c3a86e927e8
majdeddine <majda-jambouri@hotmail.fr>
2017-10-01 23:59:22 +0100

    Improved ask method

---
+++ chap09/ex1.rb
@@ -0,0 +1,14 @@
+#Improved ask method.
+def ask (question)
+  while true
+    puts question
+    reply = gets.chomp.downcase
+
+    if reply == 'yes' || reply =='no'
+      return true if reply == 'yes'
+      return false if reply  == 'no'
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+end
