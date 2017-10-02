5a7faa9ba96844033cd0f5b07775592dde56554a
majdeddine <majda-jambouri@hotmail.fr>
2017-10-02 12:33:59 +0100

    Old-school Roman numerals

--- chap09/ex2.rb
+++ chap09/ex2.rb
@@ -1,5 +1,5 @@
 #Old-school Roman numerals.
-  def dec(num)
+  def old_roman_numeral(num)
     key = [1000, 500, 100, 50, 10, 5, 1]
     value = ["M", "D", "C", "L", "X", "V", "I"]
     [*0..key.length-1].map!{|i| key[i] == 1000? value[i]*(num / key[i])  : value[i]*(num % key[i-1] / key[i])  }.join
