1497422462dab8e8ec0b27a4440cdd2070b4d050
majdeddine <majda-jambouri@hotmail.fr>
2017-10-03 14:32:25 +0100

    Sort some array

---
+++ chap10/ex1.rb
@@ -0,0 +1,16 @@
+#Sort some array
+def sort (some_array)
+  recursive_sort some_array, []
+end
+def recursive_sort(unsorted_array, sorted_array)
+  if unsorted_array == []
+    return sorted_array
+  end
+
+    smallest_word = unsorted_array.inject(unsorted_array[0]){|min, w|
+                        w <= min  ? min=w : min}
+  (unsorted_array.count(smallest_word)).times {sorted_array << smallest_word }
+  unsorted_array-=[smallest_word]
+  recursive_sort(unsorted_array, sorted_array)
+
+end
