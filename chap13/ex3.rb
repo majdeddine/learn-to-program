52a7e7adbb74d70a9f4398da1a5aa840e172dca5
majdeddine <majda-jambouri@hotmail.fr>
2017-10-08 14:50:54 +0100

    Interactive baby dragon

---
+++ chap13/ex3.rb
@@ -0,0 +1,127 @@
+#Interactive baby dragon
+class Dragon
+
+  def initialize name
+    @name = name
+    @asleep = false
+    @stuff_in_belly = 10
+    @stuff_in_intestine = 0
+    puts "#{@name} is born."
+    interactive
+  end
+
+  def interactive
+    @methods = ["feed", "walk", "put_to_bed", "toss", "rock"]
+    puts "Enter a command: #{@methods.join(", ")} Or x to exit."
+    while true
+      @command = gets.chomp
+      if @command == "x"
+        break
+      elsif @methods.include?(@command)
+        self.feed if @command == @methods[0]
+        self.walk if @command == @methods[1]
+        self.put_to_bed if @command == @methods[2]
+        self.toss if @command == @methods[3]
+        self.rock if @command == @methods[4]
+      else
+        puts "Command not found !! try again:"
+     end
+   end
+  end
+
+  def feed
+    puts "You feed #{@name}."
+    @stuff_in_belly = 10
+    passage_of_time
+  end
+
+  def walk
+    puts "You walk #{@name}."
+    @stuff_in_intestine = 0
+    passage_of_time
+  end
+
+  def put_to_bed
+    puts "You put #{@name} to bed."
+    @asleep = true
+    3.times do
+      if @asleep
+        passage_of_time
+      end
+      if @asleep
+        puts "#{@name} snores, filling the room with smoke."
+      end
+    end
+    if @asleep
+      @asleep = false
+      puts "#{@name} wakes up slowly."
+    end
+  end
+
+  def toss
+    puts "You toss #{@name} up into the air."
+    puts "He giggles, which singes your eyebrows."
+    passage_of_time
+  end
+
+  def rock
+    puts "You rock #{@name} gently."
+    @asleep = true
+    puts "He briefly dozes off..."
+    passage_of_time
+    if @asleep
+      @asleep = false
+      puts "...but wakes when you stop."
+    end
+  end
+
+
+    private
+
+    def hungry?
+      @stuff_in_belly <= 2
+    end
+
+    def poopy?
+      @stuff_in_intestine >= 8
+    end
+
+    def passage_of_time
+      if @stuff_in_belly > 0
+        @stuff_in_belly = @stuff_in_belly - 1
+        @stuff_in_intestine = @stuff_in_intestine + 1
+      else
+        if @asleep
+          @asleep = false
+          puts "He wakes up suddenly!"
+        end
+        puts "#{@name} is starving! In desperation, he ate YOU!"
+        exit
+      end
+
+      if @stuff_in_intestine >= 10
+        @stuff_in_intestine = 0
+        puts "Whoops! #{@name} had an accident..."
+      end
+
+      if hungry?
+        if @asleep
+          @asleep = false
+          puts "He wakes up suddenly!"
+        end
+        puts "#{@name}'s stomach grumbles..."
+      end
+
+      if poopy?
+        if @asleep
+          @asleep = false
+          puts "He wakes up suddenly!"
+        end
+        puts "#{@name} does the potty dance..."
+      end
+    end
+
+end
+puts "What would you like to name your dragon?"
+dragon = gets.chomp
+pet = Dragon.new dragon
